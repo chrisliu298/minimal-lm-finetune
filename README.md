f27314242f788ca12189dcb37596c08b33f7a97a
chrisliu298 <59010212+chrisliu298@users.noreply.github.com>
2024-04-05 11:14:20 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# minimal-lm-finetune
+# A Minimal Example of Fine-Tuning Language Models
+
+This repository contains a minimal example of fine-tuning autoregressive language models (e.g. Llama 2) on a language modeling task without LoRA. By default, it uses Hugging Face's [`accelerate`](https://huggingface.co/docs/accelerate/en/index) and [DeepSpeed ZeRO-3](https://huggingface.co/docs/transformers/main/en/deepspeed) to fine-tune the model on two GPUs.
+
+The code has only been tested on a machine with two NVIDIA A100 GPUs. If you have a different setup, you may need to adjust the code accordingly.
+
+## Configuration
+
+Make sure to use the latest version of `transformers` and install `accelerate` amd `deepspeed` before proceeding.
+
+```shell
+python -m pip install -U transformers
+python -m pip install accelerate deepspeed
+```
+
+The `accelerate_config.yaml` configuration file can also be obtained by running `accelerate config` from the command line. This will start an interactive prompt that will guide you through the process of creating a configuration file.
+
+For the DeepSpeed configuration, please refer to the [DeepSpeed documentation](https://www.deepspeed.ai/docs/config-json/) for a comprehensive list of options.
+
+## Usage
+
+To fine-tune the model, run the following command:
+
+```shell
+accelerate launch \
+  --config_file accelerate_config.yaml \
+  --use_deepspeed \
+  --deepspeed_config_file deepspeed_config.json \
+  finetune.py \
+  --dataset_name wikitext \
+  --subset_name wikitext-2-raw-v1 \
+  --model_name meta-llama/Llama-2-7b-hf \
+  --num_epochs 5 \
+  --lr 2e-5 \
+  --batch_size 4
+```
+
+## Notes
+
+- Do not use `device_map` or `model.to(device)` to move the model to a specific device. Accelerate will handle this for you.
+- I am using the `paged_adamw_32bit` optimizer here, because the regular `adamw_hf` won't fit on two GPUs. Note that the paged version could be slower. Feel free to try if [other optimizers](https://github.com/huggingface/transformers/blob/76fa17c1663a0efeca7208c20579833365584889/src/transformers/training_args.py#L146) work for you.
---
+++ finetune.py
@@ -0,0 +1,83 @@
+import argparse
+
+import torch
+from datasets import load_dataset
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    DataCollatorForLanguageModeling,
+    Trainer,
+    TrainingArguments,
+)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--dataset_name", type=str, required=True)
+parser.add_argument("--subset_name", type=str)
+parser.add_argument("--model_name", type=str, required=True)
+parser.add_argument("--batch_size", type=int, default=4)
+parser.add_argument("--num_epochs", type=float, default=10)
+parser.add_argument("--lr", type=float, default=2e-5)
+args = parser.parse_args()
+
+# ==============
+#  Load dataset
+# ==============
+dataset = load_dataset(args.dataset_name, args.subset_name)
+
+# ==========================
+#  Load tokenizer and model
+# ==========================
+tokenizer = AutoTokenizer.from_pretrained(args.model_name, trust_remote_code=True)
+# Set pad token to eos token if the model does not have a pad token
+if tokenizer.pad_token is None:
+    tokenizer.pad_token = tokenizer.eos_token
+model = AutoModelForCausalLM.from_pretrained(
+    args.model_name, torch_dtype=torch.bfloat16, attn_implementation="flash_attention_2"
+)
+
+# ==================
+#  Tokenize dataset
+# ==================
+dataset = dataset.map(lambda x: {"text": x["text"] + tokenizer.eos_token})
+dataset = dataset.map(lambda x: tokenizer(x["text"]), batched=True)
+
+# ===========================
+#  Define training arguments
+# ===========================
+save_dir = f"{args.model_name}_{args.dataset_name}"
+training_args = TrainingArguments(
+    output_dir=save_dir,
+    overwrite_output_dir=True,
+    do_train=True,
+    do_eval=True,
+    per_device_train_batch_size=args.batch_size,
+    per_device_eval_batch_size=args.batch_size,
+    learning_rate=args.lr,
+    lr_scheduler_type="cosine",
+    weight_decay=0.01,
+    num_train_epochs=args.num_epochs,
+    warmup_ratio=0.1,
+    logging_strategy="steps",
+    logging_first_step=True,
+    bf16=True,
+    optim="paged_adamw_32bit",
+    report_to="none",
+    save_only_model=True,
+)
+
+# =============
+#  Train model
+# =============
+# Use data collator for dynamic padding
+data_collator = DataCollatorForLanguageModeling(tokenizer, mlm=False)
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    data_collator=data_collator,
+    train_dataset=dataset["train"],
+    eval_dataset=dataset["validation"],
+)
+trainer.train()
+# Save model + tokenizer
+trainer.save_model(save_dir)
+tokenizer.save_pretrained(save_dir)
